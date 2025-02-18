48b7d47c29f21d413f646c77a12c27ce3239b4d5
p43lz3r <boris.steinmetz@gmail.com>
2025-02-18 12:32:51 +0100

    Update esp32_s3_zero_SSD1306.h

--- src/drivers/devices/esp32_s3_zero_SSD1306.h
+++ src/drivers/devices/esp32_s3_zero_SSD1306.h
@@ -4,7 +4,5 @@
 #define PIN_BUTTON_1 6
 //#define PIN_BUTTON_2 22  // Not used
 //#define PIN_ENABLE5V 21  // Not used
-//#define LED_PIN      4   // Red pin
-//#define LED_PIN_G    17  // Green pin
 
 #endif
